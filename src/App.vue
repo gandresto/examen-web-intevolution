9aa8172b1b9b7c91e1af9b947142b54b5a89445c
Andrés González <gandresto@outlook.com>
2020-10-22 13:56:18 -0500

    Agrego estructura básica de tabla

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container>
+      <b-row>
+        <b-col sm=12>
+          <b-button variant="success">
+            Añadir usuario
+          </b-button>
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col sm=12 md=3>
+          <label for="buscar-usuario">Buscar usuario</label>
+        </b-col>
+        <b-col sm=12 md=9>
+          <b-form-input id="buscar-usuario" v-model="text" placeholder="Nombre">
+          </b-form-input>
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col sm=12>
+          <b-table striped :items="usuarios"></b-table>
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col sm=12 md=3>
+          Eliminar usuarios marcados
+        </b-col>
+        <b-col sm=12 md=9>
+          <b-button id="eliminar-usuarios" variant="danger">
+            Eliminar
+          </b-button>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data(){
+    return {
+      usuarios: [
+        {
+          nombre: "Andrés",
+          edad: 26,
+          genero: "Hombre"
+        },
+        {
+          nombre: "Itzel",
+          edad: 23,
+          genero: "Mujer"
+        },
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
+  .row{
+    padding-top: 0.8rem;
+    padding-bottom: 0.8rem;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import './scss/style.scss'
+import Vuex from 'vuex'
+// import store from './store/';
+
+Vue.use(Vuex)
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
